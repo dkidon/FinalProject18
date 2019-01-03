f9d8d7fef48bb63776f4073cf9f59917a723fc08
dkidon <dkidon@ucvts.org>
2019-01-03 15:12:28 +0000

    Introductory lines of code added

---
+++ final-project.py
@@ -0,0 +1,4 @@
+print (f"Welcome traveler! You are about to embark on a long journey, one filled with peril and danger! I will be your guide through this adventure. But, before we begin...")
+name = input("What is your name?")
+print (f"Welcome", name,"! You have three options to start this adventure. You can either start as an elf, troll, or human.")
+character_class= input("What do you choose?")
