d75d3881340575a8a28bb7fbfd852ad63ae59bf5
dkidon <dkidon@ucvts.org>
2019-01-03 15:46:09 +0000

    Creating classes for the character to choose from.

--- final-project.py
+++ final-project.py
@@ -1,4 +1,8 @@
 print (f"Welcome traveler! You are about to embark on a long journey, one filled with peril and danger! I will be your guide through this adventure. But, before we begin...")
 name = input("What is your name?")
 print (f"Welcome", name,"! You have three options to start this adventure. You can either start as an elf, troll, or human.")
-character_class= input("What do you choose?")
+character_class= {"elf" , "troll", "human"}
+while true:
+    user = input("What is your choice. Elf, troll, or human?")
+    if user not in character_class:
+        print("Sorry, that's not a character choice.")
