5aaec172b80f04330ba3222263753c997f206136
dkidon <dkidon@ucvts.org>
2019-01-07 14:28:46 +0000

    Restarted code

--- final-project.py
+++ final-project.py
@@ -1,8 +1,23 @@
-print (f"Welcome traveler! You are about to embark on a long journey, one filled with peril and danger! I will be your guide through this adventure. But, before we begin...")
-name = input("What is your name?")
-print (f"Welcome", name,"! You have three options to start this adventure. You can either start as an elf, troll, or human.")
-character_class= {"elf" , "troll", "human"}
-while true:
-    user = input("What is your choice. Elf, troll, or human?")
-    if user not in character_class:
-        print("Sorry, that's not a character choice.")
+import random
+import time
+
+def intro():
+    print("Welcome traveler!")
+    print("You are about to embark on a long journey, one filled with peril and danger!")
+    print("I will be your guide through this adventure.")
+
+git add final-project.py
+git status
+git commit -m "Redone introduction for easy reading."
+git push origin master
+
+def first_room():
+    print("You wake up in a dark room.")
+    print("The walls are wooden, and you assume you are in a wood cabin.")
+    print("There are three doors, one to your left, one to your right, and one in front of you.")
+first_room_door = ["left" , "right" , "front"]
+first_room_door_choice = input(f"What is your choice")
+if first_room_door_choice not in first_room_door:
+    print(
+choice = input
+intro()
