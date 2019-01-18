43e9bb8252337922279c40d7b7271e41aa9ebb11
dkidon <dkidon@ucvts.org>
2019-01-18 14:18:19 +0000

    Continuing with classes and intro created

--- final-project.py
+++ final-project.py
@@ -24,8 +24,24 @@ class Character():
 class Story():
     def __init__(self):
         print("Welcome to the Dark Cave")
+        self.new_game()
+    def check_answer(self, question, answers):
+        answer = " "
+        while answer not in answers:
+            answer = input(question).lower()
+        return answer
+    def new_game(self):
+        self.create_char()
+        self.outside()
+    def create_char(self):
+        name = input("Please enter a name for your character: ")
+        self.pc = Character(name)
+    def outside(self):
+        print("You are a slave. Your master has brought you to the dark cave.")
+        print("Many people have died here before.")
+        print("You master commands that you enter the cave to collect the Evil book of spells.")
+
+        self.check_answer("Do you enter the cave?", ["yes" , "no"])
+
 
 new_game = Story()
-
-
-
